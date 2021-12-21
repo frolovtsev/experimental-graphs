5e9c89b1d36e5e2c5f9a49be4dfe8cc6b9552d5f
LesiaSh <93438861+LesiaSh@users.noreply.github.com>
2021-12-21 17:14:05 +0300

    сhange to curve_fit

--- Untitled5.py
+++ Untitled5.py
@@ -6,10 +6,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import openpyxl
-book = openpyxl.open(r'C:\Users\Liaisan\Desktop\data.xlsx', read_only=True)
-sheet = book.active
+from openpyxl import load_workbook
+from scipy.optimize import curve_fit
 
+book = load_workbook(r'C:\Users\Liaisan\Desktop\data.xlsx')
+sheet = book.active
+# y = kx + b
+def func(x, k, b):
+    return k*x + b
 x = []
 y = []
 xx = []
@@ -24,63 +28,21 @@ for row in range(2,sheet.max_row+1):
 x = np.array(xx)
 y = np.array(yy)
 Yerr = np.array(YYerr)
-n = sheet.max_row-1
 
-sum_x = 0;
-for i in range(n): 
-    sum_x = sum_x + x[i]
-sum_y = 0;
-for i in range(n):
-    sum_y = sum_y + y[i]
-sum_xx = 0;
-for i in range(n): 
-    sum_xx = sum_xx + x[i]**2
-sum_xy = 0;
-for i in range(n): 
-    sum_xy = sum_xy + y[i]*x[i]
-sum_yy = 0;
-for i in range(n): 
-    sum_yy = sum_yy + y[i]**2
-    
-B = (sum_y*sum_xx - sum_x*sum_xy) / (n * sum_xx - sum_x**2)
-K = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x**2)
-min0 = x.min()
-max0 = x.max()
-x0 = np.array([min0,max0])
-y0 = np.array([min0*K + B,max0*K + B])
-
+popt, pcov = curve_fit(func, x, y) 
+plt.plot(x, func(x, *popt), 'violet')
 plt.xlabel(nameX)
 plt.ylabel(nameY)
 plt.grid()
 
 plt.scatter(x, y)
-plt.plot(x0, y0, color = 'pink')
 plt.errorbar(x, y, yerr=Yerr,fmt='o')
 
 plt.show()
 
-Sum_y2 = 0;
-for i in range(n): 
-    Sum_y2 = Sum_y2 + (y[i] - B - K*x[i])**2
-Sum_y2 = Sum_y2/(n-2)
-SB = (Sum_y2*sum_xx/(n*sum_xx - sum_x**2))**0.5
-SK = (Sum_y2*n/(n*sum_xx - sum_x**2))**0.5
-
 print('y  =  kx + b')
-print( 'b  = ', B)
-print( 'Sb = ', SB)
-print( 'k  = ', K)
-print('Sk = ', SK)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
+print('k = ', popt[0])
+print('b = ', popt[1])
 
 
 
